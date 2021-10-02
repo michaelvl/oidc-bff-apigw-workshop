78663baabe837e63e1a066d28f15d455e63d06ec
MichaelVL <mvl.gh@network42.dk>
2021-10-02 17:01:02 +0200

    Add router component

---
+++ router/src/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const logger = require('morgan');
+
+const port = process.env.CLIENT_PORT || 5000;
+
+const app = express();
+app.use(logger('combined'));
+
+app.use('/login', createProxyMiddleware({
+    target: "http://localhost:5010",
+    changeOrigin: true,
+    pathRewrite: {'^/login' : ''},
+}));
+
+app.use('/api', createProxyMiddleware({
+    target: "http://localhost:5020",
+    changeOrigin: true,
+    pathRewrite: {'^/api' : ''},
+}));
+
+app.use('/', createProxyMiddleware({
+    target: "http://localhost:5030",
+    changeOrigin: true,
+}));
+
+app.listen(port, () => {
+    console.log(`Router listening on port ${port}!`);
+});
